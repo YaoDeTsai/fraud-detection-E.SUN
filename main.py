92d6f8046523d2967a650f207dbf87baa625597f
anson60214 <anson60214@gmail.com>
2023-11-12 23:40:18 -0800

    self update

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pandas as pd
-from preprocessing.data_clean import DataCleaning as clean
+from preprocessing.data_clean import DataCleaning 
 
 # read the CSV file using pandas
 train = pd.read_csv("datasets/training.csv")
 
 data_info = pd.read_excel("datasets/31_資料欄位說明.xlsx")
 
-# fill missing value
-clean.fill_csmcu_or_hcefg_acqic(train, cs_hc = "hcefg")
-clean.fill_csmcu_or_hcefg_acqic(train, cs_hc = "csmcu")
-clean.fill_stscd_neg1(train)
-clean.fill_mcc_neg1(train)
+# missing data cleaning class
+cleaner = DataCleaning(train, data_info)
+
+# fill the na by -1
+cleaner.fill_stscd_neg1()
+cleaner.fill_mcc_neg1()
+
+# fill the na by the group of acqic
+cleaner.fill_csmcu_or_hcefg_acqic(cs_hc = "hcefg")
+cleaner.fill_csmcu_or_hcefg_acqic(cs_hc = "csmcu")
 
 # target_col is the col need to fillna
 # sample_frac is the float number of proportion to sample the train data to use in RF
 # prop is the float number of selecting the train by contribute to XX% of the counts of target col's kind
-clean.fill_scity_etymd_byrf(train, data_info, "etymd", 0.2, 1.0)
-clean.fill_scity_etymd_byrf(train, data_info, "scity", 0.2, 0.9)
-clean.fill_scity_etymd_byrf(train, data_info, "stocn", 0.2, 1.0)
+cleaner.fill_scity_etymd_byrf("etymd", 0.3, 1.0)
+cleaner.fill_scity_etymd_byrf("scity", 0.3, 0.9)
+cleaner.fill_scity_etymd_byrf("stocn", 0.3, 1.0)
 
 print(train.isna().sum())
