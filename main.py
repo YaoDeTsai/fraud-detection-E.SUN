f56c3c801ce47982abc1aeabc3fab009a45b136d
蔡耀德 <105727664+YaoDeTsai@users.noreply.github.com>
2023-11-17 06:37:38 +0800

    加了一堆變數
    全跑就好 scity先不要用 類別太多

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ train = pd.read_csv("datasets/training.csv")
 public = pd.read_csv("datasets/public_processed.csv")
 df = pd.concat([train,public])
 data_info = pd.read_excel("datasets/31_資料欄位說明.xlsx")
+# df = pd.read_csv("datasets/df_fill.csv")
+
 
 # missing data cleaning class
 cleaner = DataCleaning(df, data_info)
@@ -39,28 +41,15 @@ cleaner.fill_scity_etymd_byrf("stocn", 0.3, 1.0)
 replaced_cols = ['chid', 'cano', 'mchno', 'acqic']
 df[replaced_cols] = transtr(data=df).str_trans_num(data=df,columns=replaced_cols)
 
-# 加變數
-# 時間變數(周時分秒)
-col_create(data=df).create_time()
+#先將object 轉float
+df[['etymd','stocn','scity']] = df[['etymd','stocn','scity']].astype(float)
 
 #排序
 df = df.sort_values(['locdt','hrs_loctm','chid','cano'])
 
-# 發生日期間隔
-df['diff_locdt'] = df.groupby('cano')['locdt'].diff().fillna(-1)
-
-# 0是實體交易，1是線上+沒3D驗證，2是線上+3D驗證
-df['ecfg_3dsmk'] = df['ecfg'] + df['flg_3dsmk'] 
-
-# 把stocn的冷門國家換成-1(others)
-stocn_most = (df.stocn.value_counts())[(df.stocn.value_counts()>10000)].index
-df['new_stocn'] = np.where(df['stocn'].isin(stocn_most),df['stocn'], -1)
-
-# 減少scity數量(台灣取累計盜刷比例>0.9的city換成other(-1))
-
-def tw_scity_process(data, proportion = 0.9):
+def process_tw_scity(data, proportion = 0.9):
     # 先抓台灣training資料
-    train_tw = data[(data.stocn==0)&(data.label>=0)] 
+    train_tw = data[(data.stocn==0)&(data.label.isin([0,1]))] 
     cum_fraud_tw = ((train_tw.groupby('scity')['label'].sum())/sum(train_tw.label)).sort_values(ascending=False).cumsum()
     # 取累積比例 > proportion 的 index
     twcity_others = set((data[(data.stocn==0)].scity.unique())).difference((cum_fraud_tw[cum_fraud_tw<proportion].index)) #TW所有city - TW熱門city
@@ -68,34 +57,98 @@ def tw_scity_process(data, proportion = 0.9):
     condition = (data.stocn==0) & (data.scity.isin(twcity_others))
     data.loc[condition, 'new_scity'] = -1
 
-tw_scity_process(df)
 
-# 國外scity還沒改
+df['label'] = df.label.astype(int)
+# Functions #之後再丟改preprocess啦
+# 類別資料
+def latfeature_cuncount(data:pd.DataFrame, column, feat, colname:str, shift:int, start=0):
+    data[colname] = -1
+    for t in range(start, max(data.locdt)+1):
+        if (t%7==0):print(f'{max(0,t-shift+1)}<=locdt<={t}')
+        time_intervel = (data.locdt>=(t-shift+1))&(data.locdt<=t)
+        sub_data = data[time_intervel][['locdt', column, feat]]
+        sub_result = (sub_data.groupby(column)[feat].cumcount()+1)[sub_data.locdt==t].values
+        data.loc[data['locdt'] == t, colname] = sub_result
 
-# 未完
 
-##########################################################################################################################################
-## EDA細節
-# 盜刷幾乎都發生在國外(台灣僅占7%)  # 所以按比例推算public盜刷資料應該約2500筆
-# 或許可以把模型分為國內、國外兩種
 
-# 盜刷約3成發生在第一筆交易，約5成5與發生在第一筆交易的同一天(增加diff_locdt變數)
-(df[df.label==1].diff_locdt.value_counts(normalize=True).sort_index())
+def custom_mode(x):
+    if (x.nunique() == 1): return x.iloc[0] 
+    else: return (x.mode().iloc[0])
+def latfeature_mode(data:pd.DataFrame, column, feat, colname:str, shift:int):
+    data[colname] = -1
+    for t in range(max(data.locdt)+1):
+        if (t%8==0):print(f'{max(0,t-shift+1)}<=locdt<={t}')
+        time_intervel = (data.locdt>=(t-shift+1))&(data.locdt<=t)
+        sub_data = data[time_intervel][['locdt', column, feat]]
+        sub_result = (sub_data.groupby(column)[feat].agg(lambda x: x.mode().iloc[0])).fillna(-1).values
+        sub_result = (sub_data.groupby(column)[feat].agg(custom_mode)).fillna(-1).values
+        
+        
+        data.loc[data['locdt'] == t, colname] = sub_result
 
-# 在線上交易中，沒有3D驗證盜刷筆數風險約5.7倍(增加ecfg_3dsmk變數)
-df[(df.label!=-1)&(df.ecfg_3dsmk==0)].label.mean()
-df[(df.label!=-1)&(df.ecfg_3dsmk==1)].label.mean()
-df[(df.label!=-1)&(df.ecfg_3dsmk==2)].label.mean()
 
-# 把stocn中個數小於10000的合併最好，因為這樣合併盜刷比例最高(從下面程式碼看出來的)
-a=[]
-for x in range(1,31):
-    b = 8000+1000*x
-    C = (df[df.stocn.isin((df.stocn.value_counts())[(df.stocn.value_counts()<b)].index)].label.value_counts())
-    print(b,(C[0]+C[1]))
-    a.append(round(100*C[1]/(C[0]+C[1]),6))
-    print(round(100*C[1]/(C[0]+C[1]),6),'%')
+# 數值型
+def log1p_feature(data:pd.DataFrame, column):
+    data[[f'{x}_log1p' for x in column]] = np.log1p(data[column])
 
-#國外沒有分期付款 #也不能紅利交易
-df[(df.label!=-1)&(df.stocn!=0)&(df.insfg==1)].label.value_counts(normalize=True)
-df[(df.label!=-1)&(df.stocn!=0)&(df.bnsfg==1)].label.value_counts(normalize=True)
+def latfeature_mean(data:pd.DataFrame, column, feat, colname:str, shift:int, start=0):
+    data[colname] = -1
+    data[colname] = data[colname].astype(float)
+    for t in range(start, max(data.locdt)+1):
+        if (t%7==0) : print(f'{max(0,t-shift+1)}<=locdt<={t}')
+        time_intervel = (data.locdt>=(t-shift+1))&(data.locdt<=t)
+        sub_data = data[time_intervel][['locdt', column, feat]]
+        sub_result = (sub_data.groupby(column)[feat].expanding().mean().values)[sub_data.locdt==t]
+        data.loc[data['locdt'] == t, colname] = sub_result
+
+
+
+# 把stocn的冷門國家換成-1(others)
+stocn_most = (df.stocn.value_counts())[(df.stocn.value_counts()>10000)].index
+df['new_stocn'] = np.where(df['stocn'].isin(stocn_most),df['stocn'], -1)
+
+# 減少scity數量(台灣取累計盜刷比例>0.9的city換成other(-1))
+process_tw_scity(df)
+
+## 加變數
+# 時間變數(周時分秒)
+col_create(data=df).create_time()
+
+# 發生日期間隔
+df['diff_locdt'] = df.groupby('cano')['locdt'].diff().fillna(-1)
+
+# 0是實體交易，1是線上+沒3D驗證，2是線上+3D驗證
+df['ecfg_3dsmk'] = df['ecfg'] + df['flg_3dsmk'] 
+
+# chid、cano分組近7天累積次數、ratio
+latfeature_cuncount(df,column='cano',feat='txkey',colname='cano_cumcount',shift=7)
+latfeature_cuncount(df,column='chid',feat='txkey',colname='chid_cumcount',shift=7)
+df['cards_ratio'] = (df.cano_cumcount/df.chid_cumcount)
+
+#log(x+1) + difference
+log1p_feature(df,['conam','flam1','csmam'])
+df['difflog1p_flam1conam'] = df['conam_log1p'] - df['flam1_log1p']
+latfeature_mean(df, column='cano', feat='flam1_log1p', colname='flam1avg7log1p_cano', shift=7)
+latfeature_mean(df, column='mcc', feat='flam1_log1p', colname='flam1avg7log1p_mcc', shift=7)
+log1p_feature(df,['flam1avg7_cano']) 
+df['flam1_diff_avg7log1p'] = df['flam1_log1p'] - df['flam1avg7log1p_cano']
+
+
+
+# 將指定的類別變數轉換為物件型別
+cat_cols1 = data_info[data_info['資料格式']=='類別型']['訓練資料欄位名稱'].iloc[:-2]
+cat_cols2 = ['ecfg_3dsmk','new_stocn','new_scity','weekday']
+
+df[cat_cols1] = df[cat_cols1].astype('object')
+df[cat_cols2] = df[cat_cols2].astype('object')
+
+new_train  = train[['txkey']].merge(df, how='left', on='txkey')
+new_public = public[['txkey']].merge(df, how='left', on='txkey')
+
+new_train.drop(['loctm','stocn','scity','flg_3dsmk','m_loctm','s_loctm'], axis=1,inplace=True)
+new_public.drop(['loctm','stocn','scity','flg_3dsmk','m_loctm','s_loctm','label'], axis=1,inplace=True)
+
+# df.to_csv("datasets/df_fill.csv",index=False)
+new_train.to_csv("datasets/new_train.csv",index=False)
+new_public.to_csv("datasets/new_public.csv",index=False)
